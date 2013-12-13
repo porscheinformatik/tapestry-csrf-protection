08af505186de2f31e276db41ea36bbd3cc39325c
derkoe <christian.koeberl@gmail.com>
2013-12-13 13:04:41 +0100

    CsrfTokenHolder is now Serializable, closes #1

--- src/main/java/org/apache/tapestry5/csrfprotection/internal/CsrfTokenHolder.java
+++ src/main/java/org/apache/tapestry5/csrfprotection/internal/CsrfTokenHolder.java
@@ -1,10 +1,14 @@
 package org.apache.tapestry5.csrfprotection.internal;
 
+import java.io.Serializable;
+
 /**
  * Holds the token in the user session.
  */
-public class CsrfTokenHolder
+public class CsrfTokenHolder implements Serializable
 {
+    private static final long serialVersionUID = -3238063104558657324L;
+
     private final String token;
 
     /**
